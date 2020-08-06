4ad7bd27bf4865404cfd3f6ceb2af5b80f5069fe
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2020-08-06 18:21:50 -0500

    forked it

--- src/3d/floor.js
+++ src/3d/floor.js
@@ -8,19 +8,15 @@ exports.mesh = undef;
 exports.init = init;
 
 function init() {
-    var geometry = new THREE.PlaneGeometry( 4000, 4000, 10, 10 );
-    var planeMaterial = new THREE.MeshStandardMaterial( {
+    var geometry = new THREE.PlaneGeometry(4000, 4000, 10, 10);
+    var planeMaterial = new THREE.MeshStandardMaterial({
         roughness: 0.7,
         metalness: 1.0,
         color: 0x333333,
-        emissive: 0x000000
+        emissive: 0x000000,
     });
-    var floor = exports.mesh = new THREE.Mesh( geometry, planeMaterial );
+    var floor = (exports.mesh = new THREE.Mesh(geometry, planeMaterial));
 
     floor.rotation.x = -1.57;
     floor.receiveShadow = true;
-
-
-
-
 }
